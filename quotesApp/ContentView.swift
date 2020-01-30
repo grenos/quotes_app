267908f21c878769fb1d4f72b1c4f3873c1717d8
grenos <vasilis.green@gmail.com>
2020-01-30 20:54:34 +0100

    Initial Commit

---
+++ quotesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  quotesApp
+//
+//  Created by Vasileios Gkreen on 30/01/2020.
+//  Copyright © 2020 Vasileios Gkreen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
