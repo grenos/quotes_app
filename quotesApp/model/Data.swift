41330d1d9cd15cf0c19bbba7331bbc3f86262a37
grenos <grenos>
2020-02-22 13:43:10 +0100

    first commit

---
+++ quotesApp/model/Data.swift
@@ -0,0 +1,9 @@
+//
+//  Data.swift
+//  quotesApp
+//
+//  Created by Vasileios Gkreen on 31/01/2020.
+//  Copyright © 2020 Vasileios Gkreen. All rights reserved.
+//
+
+import Foundation
---
+++ quotesApp/model/Quote.swift
@@ -0,0 +1,9 @@
+//
+//  Quote.swift
+//  quotesApp
+//
+//  Created by Vasileios Gkreen on 30/01/2020.
+//  Copyright © 2020 Vasileios Gkreen. All rights reserved.
+//
+
+import Foundation
